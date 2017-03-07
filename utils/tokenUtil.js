3c3481b86f17cd55efd955d78bd17a89839f4a25
Shivanagesh <ch.shivanagesh@gmail.com>
2017-03-07 01:03:14 -0800

    Fixed token issue

--- utils/tokenUtil.js
+++ utils/tokenUtil.js
@@ -8,32 +8,37 @@ var async = require('async');
 
 
 module.exports = {
-    autheticate: function (tokenString ,callback) {
+    autheticate: function (tokenString, callback) {
         async.waterfall([
             function (cb) {
                 var query = tokenCollection.findOne({token: tokenString});
-                query.exec(function (err,doc) {
-                    if(err) cb("null",false);
-                    else
-                        cb("null",true);
+                query.exec(function (err, doc) {
+                    if (err) cb("null", false);
+                    else {
+                        if (doc)
+                            cb("null", true);
+                        else
+                            cb("null", false);
+                    }
+
                 });
             }
-        ], function (err,result) {
+        ], function (err, result) {
             callback(result)
         });
     },
 
-    getToken : function (callback) {
+    getToken: function (callback) {
         async.waterfall([
             function (cb) {
                 var tokenObject = new tokenCollection();
                 tokenObject.token = rand.uid(32);
                 var token = false;
-                tokenObject.save(function (err){
-                    cb("null",tokenObject.token)
+                tokenObject.save(function (err) {
+                    cb("null", tokenObject.token)
                 })
             }
-        ], function (err,result) {
+        ], function (err, result) {
             callback(result)
         });
 
